1819d1d714b7ce24e1c2e730fe761e7b5ea0cc46
EmanuelAntol <84778389+EmanuelAntol@users.noreply.github.com>
2025-04-06 11:00:39 +0200

    Added count and result average functions to Database

--- src/Database.java
+++ src/Database.java
@@ -39,10 +39,55 @@ public class Database {
         return null;
     }
 
+    public float[] getrpiemer() {
+        float[] priemer = new float[2];
+        int pocetTele = 0;
+        int pocetKyber = 0;
+
+        for (Student student : databazaStudentov.values()) {
+            if (student instanceof TeleStudent) {
+                priemer[0] += student.getPriemer();
+                pocetTele++;
+            } else if (student instanceof KyberStudent) {
+                priemer[1] += student.getPriemer();
+                pocetKyber++;
+            }
+        }
+        if (pocetTele > 0) {
+            priemer[0] /= pocetTele;
+        } else {
+            priemer[0] = 0;
+        }
+
+        if (pocetKyber > 0) {
+            priemer[1] /= pocetKyber;
+        } else {
+            priemer[1] = 0;
+        }
+
+        return priemer;
+    }   
+
+    public int[] getPocetStudentov() {
+        int [] pocet = new int[2];
+
+        for (Student student : databazaStudentov.values()) {
+            if (student instanceof TeleStudent) {
+                pocet[0]++;
+            }
+            else if (student instanceof KyberStudent) {
+                pocet[1]++;
+            }
+        }
+
+        return pocet;
+    }
+
     public boolean saveStudentToFile(int ID) {
         
         return true; 
     }
+
     public static boolean saveDatabaseToFile() {
         return true; 
     }
